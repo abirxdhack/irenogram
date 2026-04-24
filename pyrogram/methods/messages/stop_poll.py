6e3f9add9482d1708c6b0941767bea99213f8892
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-04-24 09:21:46 +0600

    stop_poll.py - Fixed get_messages() with named parameters, simplified RPC call, fixed Poll._parse() call with proper users/chats dicts

--- pyrogram/methods/messages/stop_poll.py
+++ pyrogram/methods/messages/stop_poll.py
@@ -1,4 +1,3 @@
-
 from typing import Union
 
 import pyrogram
@@ -10,8 +9,7 @@ class StopPoll:
         self: "pyrogram.Client",
         chat_id: Union[int, str],
         message_id: int,
-        reply_markup: "types.InlineKeyboardMarkup" = None,
-        business_connection_id: str = None
+        reply_markup: "types.InlineKeyboardMarkup" = None
     ) -> "types.Poll":
         """Stop a poll which was sent by you.
 
@@ -32,9 +30,6 @@ class StopPoll:
             reply_markup (:obj:`~pyrogram.types.InlineKeyboardMarkup`, *optional*):
                 An InlineKeyboardMarkup object.
 
-            business_connection_id (``str``, *optional*):
-                Unique identifier of the business connection.
-
         Returns:
             :obj:`~pyrogram.types.Poll`: On success, the stopped poll with the final results is returned.
 
@@ -43,29 +38,26 @@ class StopPoll:
 
                 await app.stop_poll(chat_id, message_id)
         """
-        poll = (await self.get_messages(chat_id, message_id)).poll
+        poll = (await self.get_messages(chat_id=chat_id, message_ids=message_id)).poll
 
-        rpc = raw.functions.messages.EditMessage(
-            peer=await self.resolve_peer(chat_id),
-            id=message_id,
-            media=raw.types.InputMediaPoll(
-                poll=raw.types.Poll(
-                    id=int(poll.id),
-                    closed=True,
-                    question=raw.types.TextWithEntities(text="", entities=[]),
-                    answers=[]
-                )
-            ),
-            reply_markup=await reply_markup.write(self) if reply_markup else None
-        )
-        if business_connection_id is not None:
-            r = await self.invoke(
-                raw.functions.InvokeWithBusinessConnection(
-                    connection_id=business_connection_id,
-                    query=rpc
-                )
+        r = await self.invoke(
+            raw.functions.messages.EditMessage(
+                peer=await self.resolve_peer(chat_id),
+                id=message_id,
+                media=raw.types.InputMediaPoll(
+                    poll=raw.types.Poll(
+                        id=int(poll.id),
+                        question=raw.types.TextWithEntities(text="", entities=[]),
+                        answers=[],
+                        hash=0,
+                        closed=True,
+                    ),
+                ),
+                reply_markup=await reply_markup.write(self) if reply_markup else None
             )
-        else:
-            r = await self.invoke(rpc)
+        )
 
-        return await types.Poll._parse(self, r.updates[0], r.users)
+        users = {i.id: i for i in r.users}
+        chats = {i.id: i for i in r.chats}
+
+        return types.Poll._parse(self, r.updates[0], None, users=users, chats=chats)
