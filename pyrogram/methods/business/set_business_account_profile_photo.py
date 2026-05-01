1186279ef93ea2507253154efcaef268cefd463a
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-01 21:27:37 +0600

    Update set_business_account_profile_photo.py

--- pyrogram/methods/business/set_business_account_profile_photo.py
+++ pyrogram/methods/business/set_business_account_profile_photo.py
@@ -1,5 +1,5 @@
 
-from typing import Union, BinaryIO
+from typing import Union, BinaryIO, Optional
 
 import pyrogram
 from pyrogram import raw
@@ -8,21 +8,36 @@ class SetBusinessAccountProfilePhoto:
     async def set_business_account_profile_photo(
         self: "pyrogram.Client",
         business_connection_id: str,
-        photo: Union[str, BinaryIO] = None,
-        video: Union[str, BinaryIO] = None,
-        is_public: bool = None
+        photo: Optional[Union[str, BinaryIO]] = None,
+        video: Optional[Union[str, BinaryIO]] = None,
+        is_public: Optional[bool] = None
     ) -> bool:
         """Sets the profile photo of a managed business account.
 
+        .. include:: /_includes/usable-by/bots.rst
 
         Parameters:
-            business_connection_id (``str``): Unique identifier of the business connection.
-            photo (``str`` | ``BinaryIO``, *optional*): Photo to set. File path or binary IO.
-            video (``str`` | ``BinaryIO``, *optional*): Video to set. File path or binary IO.
-            is_public (``bool``, *optional*): Pass True to set the public photo.
+            business_connection_id (``str``):
+                Unique identifier of the business connection.
+
+            photo (``str`` | ``BinaryIO``, *optional*):
+                Profile photo to set. File path or binary IO.
+
+            video (``str`` | ``BinaryIO``, *optional*):
+                Profile video to set. File path or binary IO.
+
+            is_public (``bool``, *optional*):
+                Pass True to set the public photo.
 
         Returns:
             ``bool``: True on success.
+
+        Example:
+            .. code-block:: python
+
+                await app.set_business_account_profile_photo(connection_id, photo="photo.jpg")
+
+                await app.set_business_account_profile_photo(connection_id, video="video.mp4")
         """
         if photo:
             file = await self.save_file(photo)
