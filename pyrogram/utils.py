e75fc24fa242f0ca59d22952a70a6daa20dac582
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-01 20:43:43 +0600

    Update utils.py

--- pyrogram/utils.py
+++ pyrogram/utils.py
@@ -5,8 +5,10 @@ import hashlib
 import re
 import os
 import struct
+import io
+import pathlib
 from concurrent.futures.thread import ThreadPoolExecutor
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from getpass import getpass
 from typing import Union, List, Dict, Optional, Any, Callable, TypeVar
 from types import SimpleNamespace
@@ -23,17 +25,23 @@ PyromodConfig = SimpleNamespace(
     unallowed_click_alert_text=("[pyromod] You're not expected to click this button."),
 )
 
-async def ainput(prompt: str = "", *, hide: bool = False):
-    """Just like the built-in input, but async"""
+async def ainput(prompt: str = "", *, hide: bool = False, loop: Optional[asyncio.AbstractEventLoop] = None):
+    if isinstance(loop, asyncio.AbstractEventLoop):
+        loop = loop
+    else:
+        loop = get_event_loop()
+
     with ThreadPoolExecutor(1) as executor:
         func = functools.partial(getpass if hide else input, prompt)
-        return await asyncio.get_running_loop().run_in_executor(executor, func)
+        return await loop.run_in_executor(executor, func)
 
 def get_input_media_from_file_id(
     file_id: str,
     expected_file_type: FileType = None,
     ttl_seconds: int = None,
-    has_spoiler: bool = None
+    has_spoiler: bool = None,
+    video_cover: "raw.types.InputPhoto" = None,
+    video_start_timestamp: int = None
 ) -> Union["raw.types.InputMediaPhoto", "raw.types.InputMediaDocument"]:
     try:
         decoded = FileId.decode(file_id)
@@ -70,10 +78,33 @@ def get_input_media_from_file_id(
                 file_reference=decoded.file_reference
             ),
             spoiler=has_spoiler,
-            ttl_seconds=ttl_seconds
+            ttl_seconds=ttl_seconds,
+            video_cover=video_cover,
+            video_timestamp=video_start_timestamp
         )
 
     raise ValueError(f"Unknown file id: {file_id}")
+
+async def get_input_stargift(client: "pyrogram.Client", owned_gift_id: str) -> "raw.base.InputSavedStarGift":
+    if not isinstance(owned_gift_id, str):
+        raise ValueError(f"owned_gift_id has to be str, but {type(owned_gift_id)} was provided")
+
+    saved_gift_match = client.SAVED_GIFT_RE.match(owned_gift_id)
+    slug_match = client.UPGRADED_GIFT_RE.match(owned_gift_id)
+
+    if saved_gift_match:
+        return raw.types.InputSavedStarGiftChat(
+            peer=await client.resolve_peer(int(saved_gift_match.group(1))),
+            saved_id=int(saved_gift_match.group(2))
+        )
+    elif slug_match:
+        return raw.types.InputSavedStarGiftSlug(
+            slug=slug_match.group(1)
+        )
+    else:
+        return raw.types.InputSavedStarGiftUser(
+            msg_id=int(owned_gift_id)
+        )
 
 async def parse_messages(
     client,
@@ -241,6 +272,7 @@ MAX_CHANNEL_ID = -1000000000000
 MIN_CHAT_ID = -999999999999
 MAX_USER_ID_OLD = 2147483647
 MAX_USER_ID = 999999999999
+ZERO_CHANNEL_ID = 0
 
 def get_raw_peer_id(
         peer: Union[
@@ -249,7 +281,6 @@ def get_raw_peer_id(
             raw.base.InputPeer
         ]
     ) -> Optional[int]:
-    """Get the raw peer id from a Peer object"""
     if (
         isinstance(peer, raw.types.PeerUser)
         or isinstance(peer, raw.types.RequestedPeerUser)
@@ -274,7 +305,6 @@ def get_raw_peer_id(
     return None
 
 def get_peer_id(peer: Union[raw.base.Peer, raw.base.InputPeer]) -> int:
-    """Get the non-raw peer id from a Peer object"""
     if (
         isinstance(peer, raw.types.PeerUser)
         or isinstance(peer, raw.types.InputPeerUser)
@@ -414,15 +444,20 @@ async def parse_text_entities(
 def zero_datetime() -> datetime:
     return datetime.fromtimestamp(0, timezone.utc)
 
+def max_datetime() -> datetime:
+    return datetime(2038, 1, 19, 3, 14, 7, tzinfo=timezone.utc)
+
 def timestamp_to_datetime(ts: Optional[int]) -> Optional[datetime]:
     return datetime.fromtimestamp(ts) if ts else None
 
-def datetime_to_timestamp(dt: Optional[datetime]) -> Optional[int]:
-    if not dt:
-        return None
-    if isinstance(dt, int):
+def datetime_to_timestamp(dt: Optional[Union[datetime, timedelta]]) -> Optional[int]:
+    if isinstance(dt, timedelta):
+        return int((datetime.now() + dt).timestamp())
+    elif isinstance(dt, datetime):
+        return int(dt.timestamp())
+    elif isinstance(dt, int):
         return dt
-    return int(dt.timestamp())
+    return None
 
 async def run_sync(func: Callable[..., TypeVar("Result")], *args: Any, **kwargs: Any) -> TypeVar("Result"):
     return await asyncio.get_running_loop().run_in_executor(None, functools.partial(func, *args, **kwargs))
@@ -480,11 +515,6 @@ async def get_reply_to(
                 peer=peer,
                 story_id=reply_parameters.story_id
             )
-        elif reply_parameters.monoforum_id:
-            peer = await client.resolve_peer(reply_parameters.monoforum_id)
-            reply_to = types.InputReplyToMonoforum(
-                monoforum_peer=peer
-            )
         if direct_messages_topic_id:
             reply_to = types.InputReplyToMessage(
                 reply_to_message_id=0,
@@ -523,11 +553,19 @@ def get_first_url(text):
 
     return f"{matches[0][0]}://{matches[0][1]}{matches[0][2]}" if matches else None
 
+def get_file_name(
+    media: Union[io.BytesIO, str],
+    *,
+    file_name: str = "",
+    fallback: str = ""
+) -> str:
+    if file_name:
+        return file_name
 
-ZERO_CHANNEL_ID = 0
+    if isinstance(media, io.BytesIO):
+        return getattr(media, "name", fallback) or fallback
 
-def max_datetime() -> datetime:
-    return datetime(2038, 1, 19, 3, 14, 7, tzinfo=timezone.utc)
+    return pathlib.Path(media).name or fallback
 
 def get_event_loop():
     try:
@@ -540,15 +578,109 @@ def get_event_loop():
         asyncio.set_event_loop(loop)
     return loop
 
-async def get_input_stargift(client, owned_gift_id: int):
-    return raw.types.InputSavedStarGift(
-        id=owned_gift_id
-    )
-
 def to_nano(amount: float) -> int:
     return int(amount * 1_000_000_000)
+
+def from_nano(nano: int) -> float:
+    return nano / 1e9
 
 def get_premium_duration_month_count(days: int) -> int:
     if days <= 0:
         return 0
     return max(1, round(days / 30))
+
+def get_premium_duration_day_count(month_count: int) -> int:
+    if month_count <= 0 or month_count > 10000000:
+        return 7
+
+    return month_count * 30 + month_count // 3 + month_count // 12
+
+def obj_to_jsonvalue(obj) -> "raw.base.JsonValue":
+    if obj is None:
+        return raw.types.JsonNull()
+    elif isinstance(obj, bool):
+        return raw.types.JsonBool(value=obj)
+    elif isinstance(obj, (int, float)):
+        return raw.types.JsonNumber(value=obj)
+    elif isinstance(obj, str):
+        return raw.types.JsonString(value=obj)
+    elif isinstance(obj, (list, tuple)):
+        return raw.types.JsonArray(value=[obj_to_jsonvalue(x) for x in obj])
+    elif isinstance(obj, dict):
+        return raw.types.JsonObject(value=[raw.types.JsonObjectValue(key=k, value=obj_to_jsonvalue(v)) for k, v in obj.items()])
+
+    raise TypeError(f"Unsupported type: {type(obj)}")
+
+def jsonvalue_to_obj(obj: "raw.base.JsonValue"):
+    if isinstance(obj, raw.types.JsonNull):
+        return None
+    elif isinstance(obj, raw.types.JsonBool):
+        return obj.value
+    elif isinstance(obj, raw.types.JsonNumber):
+        return obj.value
+    elif isinstance(obj, raw.types.JsonString):
+        return obj.value
+    elif isinstance(obj, raw.types.JsonArray):
+        return [jsonvalue_to_obj(x) for x in obj.value]
+    elif isinstance(obj, raw.types.JsonObject):
+        return {o.key: jsonvalue_to_obj(o.value) for o in obj.value}
+
+    raise TypeError(f"Unsupported type: {type(obj)}")
+
+def expand_inline_bytes(bytes_data: bytes):
+    if len(bytes_data) < 3 or bytes_data[0] != 0x01:
+        return bytearray()
+
+    header = bytearray(
+        b"\xff\xd8\xff\xe0\x00\x10\x4a\x46\x49"
+        b"\x46\x00\x01\x01\x00\x00\x01\x00\x01\x00\x00\xff\xdb\x00\x43\x00\x28\x1c"
+        b"\x1e\x23\x1e\x19\x28\x23\x21\x23\x2d\x2b\x28\x30\x3c\x64\x41\x3c\x37\x37"
+        b"\x3c\x7b\x58\x5d\x49\x64\x91\x80\x99\x96\x8f\x80\x8c\x8a\xa0\xb4\xe6\xc3"
+        b"\xa0\xaa\xda\xad\x8a\x8c\xc8\xff\xcb\xda\xee\xf5\xff\xff\xff\x9b\xc1\xff"
+        b"\xff\xff\xfa\xff\xe6\xfd\xff\xf8\xff\xdb\x00\x43\x01\x2b\x2d\x2d\x3c\x35"
+        b"\x3c\x76\x41\x41\x76\xf8\xa5\x8c\xa5\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8"
+        b"\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8"
+        b"\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8\xf8"
+        b"\xf8\xf8\xf8\xf8\xf8\xff\xc0\x00\x11\x08\x00\x00\x00\x00\x03\x01\x22\x00"
+        b"\x02\x11\x01\x03\x11\x01\xff\xc4\x00\x1f\x00\x00\x01\x05\x01\x01\x01\x01"
+        b"\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08"
+        b"\x09\x0a\x0b\xff\xc4\x00\xb5\x10\x00\x02\x01\x03\x03\x02\x04\x03\x05\x05"
+        b"\x04\x04\x00\x00\x01\x7d\x01\x02\x03\x00\x04\x11\x05\x12\x21\x31\x41\x06"
+        b"\x13\x51\x61\x07\x22\x71\x14\x32\x81\x91\xa1\x08\x23\x42\xb1\xc1\x15\x52"
+        b"\xd1\xf0\x24\x33\x62\x72\x82\x09\x0a\x16\x17\x18\x19\x1a\x25\x26\x27\x28"
+        b"\x29\x2a\x34\x35\x36\x37\x38\x39\x3a\x43\x44\x45\x46\x47\x48\x49\x4a\x53"
+        b"\x54\x55\x56\x57\x58\x59\x5a\x63\x64\x65\x66\x67\x68\x69\x6a\x73\x74\x75"
+        b"\x76\x77\x78\x79\x7a\x83\x84\x85\x86\x87\x88\x89\x8a\x92\x93\x94\x95\x96"
+        b"\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4\xb5\xb6"
+        b"\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4\xd5\xd6"
+        b"\xd7\xd8\xd9\xda\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf1\xf2\xf3\xf4"
+        b"\xf5\xf6\xf7\xf8\xf9\xfa\xff\xc4\x00\x1f\x01\x00\x03\x01\x01\x01\x01\x01"
+        b"\x01\x01\x01\x01\x00\x00\x00\x00\x00\x00\x01\x02\x03\x04\x05\x06\x07\x08"
+        b"\x09\x0a\x0b\xff\xc4\x00\xb5\x11\x00\x02\x01\x02\x04\x04\x03\x04\x07\x05"
+        b"\x04\x04\x00\x01\x02\x77\x00\x01\x02\x03\x11\x04\x05\x21\x31\x06\x12\x41"
+        b"\x51\x07\x61\x71\x13\x22\x32\x81\x08\x14\x42\x91\xa1\xb1\xc1\x09\x23\x33"
+        b"\x52\xf0\x15\x62\x72\xd1\x0a\x16\x24\x34\xe1\x25\xf1\x17\x18\x19\x1a\x26"
+        b"\x27\x28\x29\x2a\x35\x36\x37\x38\x39\x3a\x43\x44\x45\x46\x47\x48\x49\x4a"
+        b"\x53\x54\x55\x56\x57\x58\x59\x5a\x63\x64\x65\x66\x67\x68\x69\x6a\x73\x74"
+        b"\x75\x76\x77\x78\x79\x7a\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x92\x93\x94"
+        b"\x95\x96\x97\x98\x99\x9a\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xb2\xb3\xb4"
+        b"\xb5\xb6\xb7\xb8\xb9\xba\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xd2\xd3\xd4"
+        b"\xd5\xd6\xd7\xd8\xd9\xda\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xf2\xf3\xf4"
+        b"\xf5\xf6\xf7\xf8\xf9\xfa\xff\xda\x00\x0c\x03\x01\x00\x02\x11\x03\x11\x00"
+        b"\x3f\x00"
+    )
+
+    footer = bytearray(b"\xff\xd9")
+
+    header[164] = bytes_data[1]
+    header[166] = bytes_data[2]
+
+    return header + bytes_data[3:] + footer
+
+def from_inline_bytes(data: bytes, file_name: str = None) -> io.BytesIO:
+    b = io.BytesIO()
+
+    b.write(data)
+    b.name = file_name or f"photo_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.jpg"
+
+    return b
