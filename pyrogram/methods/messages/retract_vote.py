88f5436de37c6054f46bd5041cb692133e596704
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-01 09:03:26 +0600

    Enhance poll parsing with user and chat data

--- pyrogram/methods/messages/retract_vote.py
+++ pyrogram/methods/messages/retract_vote.py
@@ -1,4 +1,3 @@
-
 from typing import Union
 
 import pyrogram
@@ -41,4 +40,7 @@ class RetractVote:
             )
         )
 
-        return await types.Poll._parse(self, r.updates[0], r.users)
+        users = {i.id: i for i in r.users} if hasattr(r, 'users') and r.users else {}
+        chats = {i.id: i for i in r.chats} if hasattr(r, 'chats') and r.chats else {}
+
+        return types.Poll._parse(self, r.updates[0], None, users=users, chats=chats)
