acf0cf4b9d58abc97aeb879eb76c77aa5f6849ef
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-03 00:23:37 +0600

    Update vote_poll.py

--- pyrogram/methods/messages/vote_poll.py
+++ pyrogram/methods/messages/vote_poll.py
@@ -50,4 +50,4 @@ class VotePoll:
         users = {i.id: i for i in r.users} if hasattr(r, 'users') and r.users else {}
         chats = {i.id: i for i in r.chats} if hasattr(r, 'chats') and r.chats else {}
 
-        return types.Poll._parse(self, r.updates[0], None, users=users, chats=chats)
+        return await types.Poll._parse(self, r.updates[0], None, users=users, chats=chats)
