347d9e727b217b556c35cc44d7729f4bc5ab1d69
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-03 22:24:01 +0600

    Update bytes.py

--- pyrogram/raw/core/primitives/bytes.py
+++ pyrogram/raw/core/primitives/bytes.py
@@ -1,33 +1,21 @@
-
-import struct
 from io import BytesIO
 from typing import Any
 
 from ..tl_object import TLObject
 
-_SHORT_PAD = [bytes((-(i + 1)) % 4) for i in range(256)]
-_LONG_PAD  = [bytes((-i) % 4)       for i in range(4)]
-
-_SHORT_HDR = [bytes([i]) for i in range(254)]
-_LONG_HDR  = b"\xfe"
 
 class Bytes(bytes, TLObject):
     @classmethod
     def read(cls, data: BytesIO, *args: Any) -> bytes:
-        first = data.read(1)[0]
+        length = int.from_bytes(data.read(1), "little")
 
-        if first <= 253:
-            x = data.read(first)
-
-            pad = (-(first + 1)) & 3
-            if pad:
-                data.read(pad)
-        else:
-            length = struct.unpack_from("<I", data.read(3) + b"\x00")[0]
+        if length <= 253:
             x = data.read(length)
-            pad = (-length) & 3
-            if pad:
-                data.read(pad)
+            data.read(-(length + 1) % 4)
+        else:
+            length = int.from_bytes(data.read(3), "little")
+            x = data.read(length)
+            data.read(-length % 4)
 
         return x
 
@@ -35,13 +23,15 @@ class Bytes(bytes, TLObject):
         length = len(value)
 
         if length <= 253:
-            pad_len = (-(length + 1)) & 3
-            return _SHORT_HDR[length] + value + (b"\x00" * pad_len if pad_len else b"")
-        else:
-            pad_len = (-length) & 3
             return (
-                _LONG_HDR
-                + struct.pack("<I", length)[:3]
+                bytes([length])
                 + value
-                + (b"\x00" * pad_len if pad_len else b"")
+                + bytes(-(length + 1) % 4)
+            )
+        else:
+            return (
+                bytes([254])
+                + length.to_bytes(3, "little")
+                + value
+                + bytes(-length % 4)
             )
