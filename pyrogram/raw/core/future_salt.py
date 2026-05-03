ce021d43f7118e0a5815bc78749b2e378c75edde
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-03 22:21:00 +0600

    Update future_salt.py

--- pyrogram/raw/core/future_salt.py
+++ pyrogram/raw/core/future_salt.py
@@ -1,12 +1,9 @@
-
-import struct
 from io import BytesIO
 from typing import Any
 
 from .primitives.int import Int, Long
 from .tl_object import TLObject
 
-_STRUCT_IIQ = struct.Struct("<iiq")
 
 class FutureSalt(TLObject):
     ID = 0x0949D9DC
@@ -18,12 +15,21 @@ class FutureSalt(TLObject):
     def __init__(self, valid_since: int, valid_until: int, salt: int):
         self.valid_since = valid_since
         self.valid_until = valid_until
-        self.salt        = salt
+        self.salt = salt
 
     @staticmethod
     def read(data: BytesIO, *args: Any) -> "FutureSalt":
-        vs, vu, salt = _STRUCT_IIQ.unpack(data.read(16))
-        return FutureSalt(vs, vu, salt)
+        valid_since = Int.read(data)
+        valid_until = Int.read(data)
+        salt = Long.read(data)
+
+        return FutureSalt(valid_since, valid_until, salt)
 
     def write(self, *args: Any) -> bytes:
-        return _STRUCT_IIQ.pack(self.valid_since, self.valid_until, self.salt)
+        b = BytesIO()
+
+        b.write(Int(self.valid_since))
+        b.write(Int(self.valid_until))
+        b.write(Long(self.salt))
+
+        return b.getvalue()
