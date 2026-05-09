d6304c8becae2f1c1cd0257445674d7d6c6b9eee
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-09 22:20:49 +0600

    Update __init__.py

--- pyrogram/methods/premium/__init__.py
+++ pyrogram/methods/premium/__init__.py
@@ -2,10 +2,12 @@
 from .apply_boost import ApplyBoost
 from .get_boosts_status import GetBoostsStatus
 from .get_boosts import GetBoosts
+from .get_user_chat_boosts import GetUserChatBoosts
 
 class Premium(
     ApplyBoost,
     GetBoostsStatus,
-    GetBoosts
+    GetBoosts,
+    GetUserChatBoosts,
 ):
     pass
