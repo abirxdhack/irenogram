745f1a43486e990886a2e39ff9ad8aae7a8550b1
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-05-26 06:24:09 +0600

    Update location.py

--- pyrogram/types/messages_and_media/location.py
+++ pyrogram/types/messages_and_media/location.py
@@ -3,16 +3,23 @@ import pyrogram
 from pyrogram import raw
 from ..object import Object
 
+
 class Location(Object):
     """A point on the map.
 
 
     Parameters:
-        longitude (``float``):
+        longitude (``float``):\
             Longitude as defined by sender.
 
-        latitude (``float``):
+        latitude (``float``):\
             Latitude as defined by sender.
+
+        accuracy_radius (``int``, *optional*):
+            The estimated horizontal accuracy of the location, in meters.
+
+        address (``str``, *optional*):
+            Textual description of the address. Only set for business locations.
 
         live_period (``int``, *optional*):
             Time relative to the message sending date, during which the location can be updated, in seconds.
@@ -31,6 +38,8 @@ class Location(Object):
         client: "pyrogram.Client" = None,
         longitude: float,
         latitude: float,
+        accuracy_radius: int = None,
+        address: str = None,
         live_period: int = None,
         heading: int = None,
         proximity_alert_radius: int = None,
@@ -39,6 +48,8 @@ class Location(Object):
 
         self.longitude = longitude
         self.latitude = latitude
+        self.accuracy_radius = accuracy_radius
+        self.address = address
         self.live_period = live_period
         self.heading = heading
         self.proximity_alert_radius = proximity_alert_radius
@@ -49,7 +60,27 @@ class Location(Object):
             return Location(
                 longitude=geo_point.long,
                 latitude=geo_point.lat,
+                accuracy_radius=getattr(geo_point, "accuracy_radius", None),
                 client=client
+            )
+
+    @staticmethod
+    def _parse_business(location: "raw.types.BusinessLocation") -> "Location":
+        if isinstance(location, raw.types.BusinessLocation):
+            longitude = None
+            latitude = None
+            accuracy_radius = None
+
+            if isinstance(location.geo_point, raw.types.GeoPoint):
+                longitude = location.geo_point.long
+                latitude = location.geo_point.lat
+                accuracy_radius = getattr(location.geo_point, "accuracy_radius", None)
+
+            return Location(
+                longitude=longitude,
+                latitude=latitude,
+                accuracy_radius=accuracy_radius,
+                address=location.address
             )
 
     @staticmethod
@@ -59,6 +90,7 @@ class Location(Object):
         return Location(
             longitude=media.geo.long,
             latitude=media.geo.lat,
+            accuracy_radius=getattr(media.geo, "accuracy_radius", None),
             live_period=media.period,
             heading=media.heading,
             proximity_alert_radius=media.proximity_notification_radius,
